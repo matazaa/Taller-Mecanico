64b349ffea4f5014ccfd21a29632a9851fd2d4d3
matazaa <mat.artigas@duocuc.cl>
2026-05-19 20:20:22 -0400

    eureka

---
+++ eureka-server/src/test/java/cl/duoc/taller/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.taller.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
